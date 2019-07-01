6ce71c8bfdab238b2b5455d9480fab2e12827bca
amahankali10 <amahanka@andrew.cmu.edu>
2019-06-30 21:32:39 -0400

    Training NN + Debugging

--- DQN_Fully_Connected/fully_connected_greedy.py
+++ DQN_Fully_Connected/fully_connected_greedy.py
@@ -1,8 +1,10 @@
 import random
 import numpy as np
 import pandas as pd
+import ast
 import torch
 from torch import nn
+from torch import optim
 from torch.utils.data import Dataset 
 from torch.utils.data import DataLoader 
 
@@ -33,6 +35,12 @@ for j in range(len(prereq_df.index)):
 
 #Read generated data
 student_records = pd.read_csv('generated_data.csv')
+for j in range(len(student_records.index)):
+	#Note: the lists in the csv file are read as
+	#strings. ast.literal_eval() converts these strings
+	#to int lists.
+	student_records.at[j, 'Trace of Learning'] = ast.literal_eval(student_records.at[j, 'Trace of Learning'])
+	student_records.at[j, 'Learning Time'] = ast.literal_eval(student_records.at[j, 'Learning Time'])
 
 #Estimate time taken by KPs from generated data
 KP_times = []
@@ -62,7 +70,8 @@ class DQNDataset(Dataset):
 	def __init__(self, student_records):
 		self.transition_num = 0
 		self.current_state = []
-		self.next_KP = []
+		self.next_state = []
+		self.new_KP = []
 
 		for j in range(len(student_records.index)):
 			current_row = student_records.iloc[j]
@@ -72,7 +81,7 @@ class DQNDataset(Dataset):
 
 			current_state_vec = []
 			for i in range(num_KP):
-				current_state_vec.append(0)
+				current_state_vec.append(0.0)
 			current_state_vec.append(budget_constraint)
 
 			for k in range(len(KPs_learned)):
@@ -80,14 +89,21 @@ class DQNDataset(Dataset):
 				used_time = time_taken[k]
 
 				self.current_state.append(current_state_vec)
-				self.next_KP.append(additional_KP)
+				self.new_KP.append(additional_KP)
+				self.transition_num += 1
 
 				current_state_vec = current_state_vec.copy()
 				current_state_vec[additional_KP] = 1
-				current_state_vec[len(current_state_vec) - 1] -= used_time  ##Updating budget
+				current_state_vec[-1] -= used_time  ##Updating budget
+				self.next_state.append(current_state_vec)
+				current_state_vec = current_state_vec.copy()
+
+		self.current_state = torch.tensor(self.current_state)
+		self.next_state = torch.tensor(self.next_state)
+		self.new_KP = torch.tensor(self.new_KP)
 
 	def __getitem__(self, index):
-		return last_state[index], next_KP[index]
+		return self.current_state[index], self.next_state[index], self.new_KP[index]
 
 	def __len__(self):
 		return self.transition_num
@@ -97,15 +113,60 @@ Define and train the Q-network.
 Here, the inputs are:
 - The binary vector describing the KPs learned so far.
 - The budget constraint
-- A one-hot vector giving the next KP.
+The output is:
+- A vector of length num_KP that gives the value of learning each new KP.
 """
+def create_q_network():
+	return nn.Sequential(nn.Linear(num_KP + 1, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, 50),
+						 nn.ReLU(),
+						 nn.Linear(50, num_KP),
+						 nn.ReLU())
+Q_network = create_q_network()
+#TODO: See the effect of an additional target network on performance
+
+#Training
+transition_list = DQNDataset(student_records)
+replay_buffer = DataLoader(dataset=transition_list, batch_size=1, shuffle=True)
+optimizer = optim.SGD(Q_network.parameters(), lr=0.01, momentum=0.4)
+loss_fn = nn.MSELoss()
+
+for epoch in range(5):
+	print("Starting %dth epoch" % epoch)
+	for current_state, next_state, new_KP in replay_buffer:
+		target = 0
+		action_values = Q_network(next_state)
+		for i in range(num_KP):
+			#action_values has shape [1, 25], rather than [25]
+			target = max(target, action_values[0, i].item())
+		target += KP_utils[new_KP]
+		#At this point, target is a scalar, rather than a 
+		#Pytorch tensor. It is not connected to Q_network. 
+		#Therefore, backpropagation will not cause gradients 
+		#to go through target.
+
+		#Required in order to use loss_fn
+		target = torch.tensor([target], dtype=torch.float32)
+
+		prediction = Q_network(current_state)
+		loss = loss_fn(prediction[0, new_KP], target)
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+print("Training complete")
 
 
-
-
-
-
-
+#Testing on this graph, for various budget constraints
 
 
 
