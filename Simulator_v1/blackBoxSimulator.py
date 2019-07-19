4a643f24b7caa128de6bc5b21c024828ae622357
amahankali10 <amahanka@andrew.cmu.edu>
2019-07-19 13:13:06 -0400

    starting simulator

---
+++ Simulator_v1/blackBoxSimulator.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+class Student:
+	def __init__(self):
+		pass
+
+	def watch_video(self, video_id):
+		pass
+
+	def take_assessment(self, test_id):
+		pass
+
+
+def generate_episode():
+	pass
+
+def create_data_set(num_episodes, filename):
+	pass
+
